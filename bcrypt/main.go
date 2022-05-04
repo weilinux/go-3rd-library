5e4149dd3eb1f1d68c7148e93a6ee204ce174211
Administrator <admin@example.com>
2022-05-04 19:00:54 +0800

    02-bcrypt HashPassword

--- bcrypt/main.go
+++ bcrypt/main.go
@@ -1,1 +1,34 @@
-package bcrypt
+package main
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(hash string, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func main() {
+	password := "123456"
+	hash, _ := HashPassword(password)
+
+	fmt.Println("Password: ", password)
+	fmt.Println("Hash: ", hash)
+
+	match := CheckPasswordHash(hash, password)
+	fmt.Println("Match: ", match)
+
+
+
+
+
+
+
+}
